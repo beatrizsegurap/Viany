7dfd6f64039cc0a846251ca301a0d864a1d603d8
javiers1999 <javier.rodriguez.s@mail.pucv.cl>
2022-12-07 20:55:14 -0300

    cambios gastronomia

--- gastronomia/forms.py
+++ gastronomia/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import gastronomia_por_dia
 
-class formAct(forms.ModelForm):
+class formAddGastronomia(forms.ModelForm):
     class Meta:
         model = gastronomia_por_dia
         fields = ['detalle_gastronomia', 'hora_inicio_gastronomia', 'hora_final_gastronomia', 'valor_total_gastronomia', 'fecha_gastronomia']
--- gastronomia/views.py
+++ gastronomia/views.py
@@ -5,7 +5,17 @@ from .forms import formAct
 # Create your views here.
 
 def gastronomia(request):
-    return render(request,"gastronomia.html")
+    if request.method == 'POST':
+        form = formAddGastronomia(request.POST or None)
+        if form.is_valid():
+            form.save()
+            request.session['fecha_gastronomia'] = form.data.get('fecha_gastronomia')
+            request.session['detalle_gastronomia'] = form.data.get('detalle_gastronomia')
+            request.session['hora_inicio_gastronomia'] = form.data.get('hora_inicio_gastronomia')
+            request.session['hora_final_gastronomia'] = form.data.get('hora_final_gastronomia')
+            request.session['valor_total_gastronomia'] = form.data.get('valor_total_gastronomia')
+            form = formAddGastronomia()
+    return redirect("/gastronomia/menu")
 
 def agregarGastronomia(request):
     return render(request,'agregar-gastronomia.html')
